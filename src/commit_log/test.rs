1201219b6d45bf0261a1bf1a48d22aa3689e79d0
Marcelo Boeira <me@marceloboeira.com>
2019-05-12 11:40:45 +0200

    Add commends to the global testing functions

--- src/commit_log/test.rs
+++ src/commit_log/test.rs
@@ -4,6 +4,14 @@ use self::rand::{distributions, thread_rng, Rng};
 use std::env;
 use std::path::PathBuf;
 
+/// Test Utilities
+///
+
+/// Random Hash for testing purposes
+///
+/// Returns a string with random alphanumerical characters
+///
+/// e.g.: 5V0eWCjkEyO8HEbhGbMuG8A2104Km
 pub fn random_hash() -> String {
     thread_rng()
         .sample_iter(&distributions::Alphanumeric)
@@ -11,6 +19,11 @@ pub fn random_hash() -> String {
         .collect()
 }
 
+/// Temp file path
+///
+/// Returns a unexistent temporary path for testing purposes
+///
+/// e.g.: /tmp/_/5V0eWCjkEyO8HEbhGbMuG8A2104Km
 pub fn tmp_file_path() -> PathBuf {
     let mut tmp_dir = env::temp_dir();
     tmp_dir.push(random_hash());
